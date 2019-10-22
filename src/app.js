70562dda0805ff2b71bdfb12b5f1fb05d42f4c2e
Ross Lorimer <wross.lorimer@googlemail.com>
2019-10-22 22:58:21 +0100

    drop down of currency codes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 import Vue from "vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#app",
+    data: {
+      currencies: {},
+      selectedCurrency: null,
+    },
+    mounted(){
+      this.fetchCurrencies();
+    },
+    methods:{
+      fetchCurrencies: function() {
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => this.currencies = data.rates)
+        
+      }
+    }
+
+
+
+
+
+
+
+  })
+})
